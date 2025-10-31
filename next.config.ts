131942a7469a6932c8d5d6bc822f0f702707c028
Craig-Solis <csolis5454@gmail.com>
2025-10-31 13:47:38 -0500

    Try This

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  allowedDevOrigins: ["10.0.2.120", "localhost", "127.0.0.1"],
 };
 
 export default nextConfig;
